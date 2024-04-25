9e2cb0b040f87ec732c3622b0e57b6178ce46c89
Mateusz Szustkiewicz <matszu0@gmail.com>
2024-04-25 21:58:04 +0100

    Integrate Tailwind CSS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}"
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
 
